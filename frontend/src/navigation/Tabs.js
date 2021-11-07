a34e447f77998f95a8ae322d1915e36dc8db1a0a
Samhith Kakarla <samhith.kakarla@gmail.com>
2021-11-07 13:10:44 -0500

    styled bottom tabs

--- frontend/src/navigation/Tabs.js
+++ frontend/src/navigation/Tabs.js
@@ -2,25 +2,26 @@ import React from 'react';
 
 import { NavLink } from 'react-router-dom';
 import { FaHome, FaCamera } from 'react-icons/fa';
+import './Tabs.css';
 
 const TabsNavigation = () => {
   return (
-    <nav className="navbar fixed-bottom navbar-light d-block d-lg-none bottom-tab-nav" role="navigation">
+    <nav className="bg-gray-two fixed-bottom px-96" role="navigation">
       {/* <Nav className="w-100"> */}
-        <div className=" d-flex flex-row justify-content-around w-100">
+        <div className="flex flex-row justify-between w-100">
           {/* <NavItem key="Home-Tab"> */}
-            <NavLink to="/home" className="nav-link bottom-nav-link" activeClassName="active">
-              <div className="row d-flex flex-column justify-content-center align-items-center">
-                <FaHome />
-                <div className="bottom-tab-label">Home</div>
+            <NavLink to="/home" className="nav-link bottom-nav-link transition duration-200 ease-in-out" activeClassName="active">
+              <div className="flex justify-center items-center">
+                <FaHome size={25} className="mt-5 mb-2" />
+                {/* <div className="bottom-tab-label">Home</div> */}
               </div>
             </NavLink>
           {/* </NavItem> */}
           {/* <NavItem key="Camera-Tab"> */}
-            <NavLink to="/camera" className="nav-link bottom-nav-link" activeClassName="active">
-              <div className="row d-flex flex-column justify-content-center align-items-center">
-                <FaCamera />
-                <div className="bottom-tab-label">Camera</div>
+            <NavLink to="/camera" className="nav-link bottom-nav-link transition duration-200 ease-in-out" activeClassName="active">
+              <div className="flex justify-center items-center">
+                <FaCamera size={25} className="mt-5 mb-2" />
+                {/* <div className="bottom-tab-label">Camera</div> */}
               </div>
             </NavLink>
           {/* </NavItem> */}
